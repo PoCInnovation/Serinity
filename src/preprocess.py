6cd62be520b1d4ad2f9d9f1103ac1639b9e3d7d3
David <david.gozlan@epitech.eu>
2021-06-15 15:50:44 +0200

    (Feat) - Some little data preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,9 @@
-import eeglib
+
+from eeglib.helpers import CSVHelper
+
+data_filepath = "./data/Test/"
+
+helper = CSVHelper(data_filepath + "Test_EPOCFLEX_131982_2021.06.08T10.12.3002.00.md.bp.csv")
+
+for eeg in helper:
+    print(eeg.PFD())
