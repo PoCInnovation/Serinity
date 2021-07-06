9d3a5c8edc9f5e60d1e7073fbb9b373e9dbc7045
Theo <t.fourcat@outlook.com>
2021-07-06 09:00:21 +0200

    Adding C label and ShallowConvNet model.

--- src/train.py
+++ src/train.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 import os
-import sklearn
 import numpy as np
-import argparse
 import tensorflow as tf
 from EEGNet import EEGNet
 from sklearn.model_selection import train_test_split
 
 #from mlflow import log_metric, log_param, log_artifacts
 
-LABELS = ['A', 'B']
+LABELS = ['A', 'B', 'C']
 training_data_path = 'data/train'
 validation_data_path = 'data/validation'
 model_path = 'models'
@@ -22,7 +20,7 @@ entries_per_sample = 500 # Nb entries for each sample
 def getData(path):
     data = []
     labels = []
-    for label in LABELS:
+    for label_nb, label in enumerate(LABELS):
         label_dir = os.path.join(path, label)
         for session_file in os.listdir(label_dir):
             filepath = os.path.join(label_dir, session_file)
@@ -30,10 +28,7 @@ def getData(path):
             splited_data = tf.split(tf.transpose(file), int(23000 / 500), axis=1)
             for sample in splited_data:
                 data.append(sample)
-                if label == LABELS[0]:
-                    labels.append(tf.constant(0))
-                elif label == LABELS[1]:
-                    labels.append(tf.constant(1))
+                labels.append(tf.constant(label_nb))
     print('Loaded:', len(data), 'samples', '\nShape:', data[0].shape, '\n\n------\n\n')
     return data, labels
 
@@ -58,13 +53,13 @@ if __name__ == "__main__":
     model = EEGNet(nb_classes=nb_labels, Chans=nb_electrodes, Samples=entries_per_sample)
 
     #Binary crossentropy (two labels)
-    model.compile(loss = 'binary_crossentropy', optimizer = 'Adam',
+    model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam',
                     metrics=['accuracy'])
 
     model.fit(tf.expand_dims(training_data, 3),
-              tf.keras.utils.to_categorical(training_labels, 2), callbacks=[cp_callback], epochs=50)
+              tf.keras.utils.to_categorical(training_labels, len(LABELS)), callbacks=[cp_callback], epochs=50)
 
     loss, accuracy = model.evaluate(tf.expand_dims(test_data, 3),
-                                    tf.keras.utils.to_categorical(test_labels, 2))
+                                    tf.keras.utils.to_categorical(test_labels, len(LABELS)))
 
     model.save("models/EEGNet_accuracy_" + str(accuracy))
